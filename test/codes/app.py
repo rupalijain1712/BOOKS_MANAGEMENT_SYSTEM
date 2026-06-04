f7ea84349b0558df4020e1a1222f44e018794d08
Sayar Sekhar Ghosh <sayarsekharghosh@gmail.com>
2026-06-04 08:27:37 +0530

    Committed on 08:27

--- test/codes/app.py
+++ test/codes/app.py
@@ -3,7 +3,10 @@ import sys
 import os
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 
-from datetime import datetime, timezone
+from datetime import datetime
+import pytz
+
+IST = pytz.timezone('Asia/Kolkata')
 
 from flask_login import (
     LoginManager,
@@ -205,8 +208,8 @@ def edit_book(id):
             book.stock = int(request.form["stock"])
             book.category = request.form["category"]
             
-            # Update the timeline property dynamically on change
-            book.updated_at = datetime.now(timezone.utc)
+            # Update the timeline property dynamically on change to IST
+            book.updated_at = datetime.now(IST)
             
             db.session.commit()
             return redirect(url_for("admin_dashboard"))
